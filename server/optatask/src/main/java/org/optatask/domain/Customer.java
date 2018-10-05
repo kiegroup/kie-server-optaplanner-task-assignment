84807672329fa323b7fbfff667cba2022b030367
Musa Talluzi <musa.talluzi@gmail.com>
2018-10-05 10:58:47 -0400

    Copy taskassignment domain from optaplanner-examples

---
+++ server/optatask/src/main/java/org/optatask/domain/Customer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optatask.domain;
+
+public class Customer extends AbstractPersistable {
+
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+    public String getLabel() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ server/optatask/src/main/java/org/optatask/domain/solver/StartTimeUpdatingVariableListener.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optatask.domain.solver;
+
+import java.util.Objects;
+
+import org.optaplanner.core.impl.domain.variable.listener.VariableListener;
+import org.optaplanner.core.impl.score.director.ScoreDirector;
+import org.optatask.domain.Task;
+import org.optatask.domain.TaskOrEmployee;
+
+public class StartTimeUpdatingVariableListener implements VariableListener<Task> {
+
+    @Override
+    public void beforeEntityAdded(ScoreDirector scoreDirector, Task task) {
+        // Do nothing
+    }
+
+    @Override
+    public void afterEntityAdded(ScoreDirector scoreDirector, Task task) {
+        updateStartTime(scoreDirector, task);
+    }
+
+    @Override
+    public void beforeVariableChanged(ScoreDirector scoreDirector, Task task) {
+        // Do nothing
+    }
+
+    @Override
+    public void afterVariableChanged(ScoreDirector scoreDirector, Task task) {
+        updateStartTime(scoreDirector, task);
+    }
+
+    @Override
+    public void beforeEntityRemoved(ScoreDirector scoreDirector, Task task) {
+        // Do nothing
+    }
+
+    @Override
+    public void afterEntityRemoved(ScoreDirector scoreDirector, Task task) {
+        // Do nothing
+    }
+
+    protected void updateStartTime(ScoreDirector scoreDirector, Task sourceTask) {
+        TaskOrEmployee previous = sourceTask.getPreviousTaskOrEmployee();
+        Task shadowTask = sourceTask;
+        Integer previousEndTime = (previous == null ? null : previous.getEndTime());
+        Integer startTime = calculateStartTime(shadowTask, previousEndTime);
+        while (shadowTask != null && !Objects.equals(shadowTask.getStartTime(), startTime)) {
+            scoreDirector.beforeVariableChanged(shadowTask, "startTime");
+            shadowTask.setStartTime(startTime);
+            scoreDirector.afterVariableChanged(shadowTask, "startTime");
+            previousEndTime = shadowTask.getEndTime();
+            shadowTask = shadowTask.getNextTask();
+            startTime = calculateStartTime(shadowTask, previousEndTime);
+        }
+    }
+
+    private Integer calculateStartTime(Task task, Integer previousEndTime) {
+        if (task == null || previousEndTime == null) {
+            return null;
+        }
+        return Math.max(task.getReadyTime(), previousEndTime);
+    }
+}
