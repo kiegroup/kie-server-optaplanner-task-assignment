de73b281629d79aed1aec7a9064b5e6c37b7ca24
Musa Talluzi <mtalluzi@redhat.com>
2018-11-09 19:10:47 -0500

    Test fetching kie server info from kie-server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const baseURI = ' http://localhost:8080/kie-server/services/rest/server';
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { info: '' };
+  }
+
+  componentDidMount() {
+    fetch(baseURI, {
+      credentials: 'include',
+      headers: {
+        'X-KIE-ContentType': 'JSON'
+      }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          var error = new Error(response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;  
+        }
+      },
+        error => { throw new Error(error.message) }
+      )
+      .then(response => this.setState({info: JSON.stringify(response)}))
+      .catch(error => console.log('Caught error: ' + error));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.info}
       </div>
     );
   }
