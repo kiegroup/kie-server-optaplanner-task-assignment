4b47f845772843b6ef79b95b1603e36bcfbc3f59
Musa Talluzi <mtalluzi@redhat.com>
2018-11-27 13:14:36 -0500

    Edit code style to comply with Airbnb ESLint code style

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path="/home" component={Home} />
-                    <Redirect to="/home" />
-                </Switch>
-            </div >
-        );
-    }
-}
+const Main = () => (
+  <div>
+    <Header />
+    <Switch>
+      <Route path="/home" component={Home} />
+      <Redirect to="/home" />
+    </Switch>
+  </div>
+);
 
 export default Main;
