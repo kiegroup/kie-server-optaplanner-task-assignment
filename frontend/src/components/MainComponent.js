96fb1f46d88ab72cbf1eaef929910d01d57a1c78
Musa Talluzi <mtalluzi@redhat.com>
2019-01-17 13:05:04 -0500

    Fix lint errors

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -85,40 +85,36 @@ class Main extends Component {
         <Switch>
           <Route
             path="/home"
-            render={(props) => {
-              return (
-                <Home
-                  {...props}
-                  container={this.state.container}
-                  solver={this.state.solver}
-                  onContainerDeployed={this.onContainerDeployed}
-                  onContainerDeleted={this.onContainerDeleted}
-                  isContainerDeployed={this.state.isContainerDeployed}
-                  bestSolution={this.state.bestSolution}
-                  score={this.state.score}
-                  updateBestSolution={this.updateBestSolution}
-                />
-              );
-            }}
+            render={props => (
+              <Home
+                {...props}
+                container={this.state.container}
+                solver={this.state.solver}
+                onContainerDeployed={this.onContainerDeployed}
+                onContainerDeleted={this.onContainerDeleted}
+                isContainerDeployed={this.state.isContainerDeployed}
+                bestSolution={this.state.bestSolution}
+                score={this.state.score}
+                updateBestSolution={this.updateBestSolution}
+              />
+            )}
           />
           <Route
             exact
             path="/tasks"
-            render={(props) => {
-              return (
-                <TaskPage
-                  {...props}
-                  tasks={this.state.bestSolution.taskList ? this.state.bestSolution.taskList : []}
-                  taskTypes={this.state.bestSolution.taskTypeList
-                    ? this.state.bestSolution.taskTypeList : []}
-                  customers={this.state.bestSolution.customerList
-                    ? this.state.bestSolution.customerList : []}
-                  container={this.state.container}
-                  solver={this.state.solver}
-                  updateBestSolution={this.updateBestSolution}
-                />
-              )
-            }}
+            render={props => (
+              <TaskPage
+                {...props}
+                tasks={this.state.bestSolution.taskList ? this.state.bestSolution.taskList : []}
+                taskTypes={this.state.bestSolution.taskTypeList
+                  ? this.state.bestSolution.taskTypeList : []}
+                customers={this.state.bestSolution.customerList
+                  ? this.state.bestSolution.customerList : []}
+                container={this.state.container}
+                solver={this.state.solver}
+                updateBestSolution={this.updateBestSolution}
+              />
+            )}
           />
           <Redirect to="/home" />
         </Switch>
